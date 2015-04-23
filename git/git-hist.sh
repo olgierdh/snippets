9ea0fb535ae84f590d54f7e5cc1c668e8d4f0fee
olgierdh <olgierd.humenczuk@logmein.com>
2015-04-23 12:38:33 +0200

    [feat] new script for printing all merges ( features, fixes ) in a given time window

---
+++ git/git-hist.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+OPTIND=1
+
+usage() { echo "Usage: $0 -s <YY:MM:DDTHH:MM:SS[TIME ZONE]> [-h (help)] [-u <date until> (By default this is an actual date)]" 1>&2; exit 1;  }
+
+since=""
+until=`date +"%Y-%m-%dT%H:%M:%S%z"`
+
+echo $NOW
+
+!(($#)) && usage 
+
+while getopts "h?s:u::" opt; do
+    case "$opt" in
+    h|\?)
+        usage
+        exit 0
+        ;;
+    s) since=$OPTARG
+        ;;
+    u) until=$OPTARG
+        ;;
+    *) usage
+        ;;
+    esac
+done
+
+git log --pretty=oneline --merges --first-parent --format="%C(auto) %h %C(green) %an %C(reset) %ad %C(reset) %s" --since="$since" --until="$until"
